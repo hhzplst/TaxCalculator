eaadaeba82c6880f9bf710694f7f61edf4fb25ec
Ivy Chang <ivycwq@icloud.com>
2016-09-28 11:57:48 -0700

    updating test cases

--- TaxCalculatorTest.cs
+++ TaxCalculatorTest.cs
@@ -4,8 +4,8 @@ namespace TaxCalculator {
     public class TaxCalculatorTest {
         public static void Main(string[] args) {
             TaxCalculator myCalculator = new TaxCalculator();
-            Console.WriteLine(myCalculator.getCurrentIncomeLimit(2));
-            Console.WriteLine(myCalculator.getCurrentIncomeLimit(6));
+            //interval from 1-6
+            myCalculator.PrintDataTable();
         }
     }
 }
