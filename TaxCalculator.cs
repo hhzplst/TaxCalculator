3b94bbdf4332a311b81afe3bfeb131015fc4696e
Ivy Chang <ivycwq@icloud.com>
2016-09-28 09:49:50 -0700

    exception handling

--- TaxCalculator.cs
+++ TaxCalculator.cs
@@ -3,10 +3,10 @@ using System;
 namespace TaxCalculator {
   public class TaxCalculator : Exception {
     private static double[] defaultIncomeLimits = new double[] {30000, 50000, 100000, 200000, 250000};
-    private static double[] defaultTaxRates = new double[] {0, 0.1, 0.2, 0.3, 0.35, 0.4};
+    private static double[] defaultTaxRates = new double[] {0.1, 0.2, 0.3, 0.35, 0.4};
     private double[] currentIncomeLimits, currentTaxRates;
     public double GrossIncome {get; set;}
-    public double Tax {get;}
+    public double Tax {get; set;}
     public double NetIncome {get;}
     public TaxCalculator() {
       Init();
@@ -20,16 +20,14 @@ namespace TaxCalculator {
       try {
         return currentIncomeLimits[interval-1];
       } catch (System.IndexOutOfRangeException ex) {
-        System.ArgumentException argEx = new ArgumentException("Error! Interval between 1-5", ex);
-        throw argEx;
+        throw new ArgumentOutOfRangeException("Error! Interval between 1-5", ex);
       }  
     }
     public void setCurrentIncomeLImit(int interval, double newLimit) {
       try {
         currentIncomeLimits[interval-1] = newLimit;
       } catch (System.IndexOutOfRangeException ex) {
-        System.ArgumentException argEx = new ArgumentException("Error! Interval between 1-5", ex);
-        throw argEx;
+        throw new ArgumentOutOfRangeException("Error! Interval between 1-5", ex);
       }  
     }
   
