89cd6512dc363bed528866e22347fa362611d284
Ivy Chang <ivycwq@icloud.com>
2016-09-28 09:49:59 -0700

    adding test cases

---
+++ TaxCalculatorTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TaxCalculator {
+    public class TaxCalculatorTest {
+        public static void Main(string[] args) {
+            TaxCalculator myCalculator = new TaxCalculator();
+            Console.WriteLine(myCalculator.getCurrentIncomeLimit(2));
+            Console.WriteLine(myCalculator.getCurrentIncomeLimit(6));
+        }
+    }
+}
