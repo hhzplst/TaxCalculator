0fcc22d3395ac2839c216dd32db46f2af9606bd3
Ivy Chang <ivycwq@icloud.com>
2016-09-29 10:48:59 -0700

    fixing bug in the algorithm

--- DataValidator.cs
+++ DataValidator.cs
@@ -1,18 +1,19 @@
 namespace TaxCalculator {
   public class DataValidator {
     public static int UpdateData(double[] arr, int index, double newValue) {
-      if (index > arr.Length - 1) return 0;
-      if (index == 0)
-        if (newValue > arr[index + 1]) return 0;
-      else if (index == arr.Length - 1)
-        if (newValue < arr[index - 1]) return 0;
-      else
-        if (newValue < arr[index - 1] || newValue > arr[index + 1]) return 0;
-
+      if (index > arr.Length - 1)
+        throw new System.IndexOutOfRangeException("Error! Interval between 1-6");
+      if (index == 0) {
+        if (arr[index + 1] < newValue) return 0;
+      } else if (index == arr.Length - 1) {
+        if (arr[index - 1] > newValue) return 0;
+      } else {
+        if (arr[index - 1] > newValue || arr[index + 1] < newValue) return 0;
+      }
       return 1;
     }
     public static bool IsValid(double[] arr, int index, double newValue) {
-      return (UpdateData(arr, index, newValue) == 1)? true:false;
+      return (UpdateData(arr, index, newValue) == 1)? true : false;
     }
   }
 }
